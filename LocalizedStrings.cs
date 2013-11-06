32f6e3ae996cb9da56aaf0169f8d2338a6379f58
Davide Setti <setti.davide89@gmail.com>
2013-11-06 10:19:58 +0100

    Commit iniziale
    Signed-off-by: Davide Setti <setti.davide89@gmail.com>

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppSDEM.Resources;
+
+namespace AppSDEM
+{
+    /// <summary>
+    /// Fornisce l'accesso alle risorse stringa.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
