0778df5aa7099afa4ca9407822adbeb094bbb03f
xhensila <xhensila88@gmail.com>
2013-11-15 12:46:08 +0100

    Login Page Provvisorio

---
+++ login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace AppSDEM
+{
+    public partial class login : PhoneApplicationPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
