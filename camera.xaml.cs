e625cb302477cda5fb4eecf47b1ecc5dc6181c74
Xhensila Doda <xhensiladoda@users.noreply.github.com>
2016-05-30 18:18:34 +0200

    Update camera.xaml.cs

--- camera.xaml.cs
+++ camera.xaml.cs
@@ -14,7 +14,7 @@ namespace AppSDEM
 {   
     /**
     * Controller responsabile per l'accesso alla fotocamera.
-    * @author Doda Xhensila e Chiara Ferrari
+    * @author Doda Xhensila
     */
     public partial class camera : PhoneApplicationPage
     {
